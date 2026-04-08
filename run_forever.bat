0752027f67c7075727d7447c1435cc09c897f7c5
topvdmm999 <topvdmm999@gmail.com>
2026-04-09 03:59:45 +0700

    Split HTML and Python

--- run_forever.bat
+++ run_forever.bat
@@ -1,7 +1,8 @@
+@echo off
 :loop
 python super_scraper.py
 git add .
-git commit -m "Auto-update from Local Server"
-git push
+git commit -m "Auto-update prices"
+git push origin main --force
 timeout /t 600
 goto loop
